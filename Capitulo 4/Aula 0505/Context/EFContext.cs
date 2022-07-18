fc56e8f6862f47dc9ed232d68e08384ce369f77a
20201011110024 <20201011110024@CNAT391770>
2022-07-18 10:24:45 -0300

    Adicionando produtos.

---
+++ Capitulo 4/Aula 0505/Context/EFContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Aula_0505.Models;
+
+namespace Aula_0505.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+
+    }
+}
