0022f40bb0e3c5ecbe186a7ce8eb13d7b5deb8dd
20201011110024 <20201011110024@CNAT391770>
2022-08-01 08:28:46 -0300

    Visão, controlador e modelo de produtos.

--- Capitulo 4/Aula 0505/Context/EFContext.cs
+++ Capitulo 4/Aula 0505/Context/EFContext.cs
@@ -9,10 +9,14 @@ namespace Aula_0505.Context
 {
     public class EFContext : DbContext
     {
-        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public EFContext() : base("Asp_Net_MVC_CS") {
+            Database.SetInitializer<EFContext>(
+                new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
 
     }
 }
